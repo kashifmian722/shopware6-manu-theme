9c22aed4106f9738e4ab707a39fef48c0fe4b0f9
Kashif <Kashifmian722@live.com>
2022-09-18 12:13:27 +0500

    merchant files updated

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -2,8 +2,6 @@ import InputSpinner from './js/input-spinner/input-spinner.plugin';
 import NewsletterPrivacy from './js/newsletter-privacy/newsletter-privacy.plugin';
 import TogglePassword from './js/toggle-password/toggle-password.plugin';
 import RegisterExpand from './js/register-expand/register-expand.plugin';
-import ExtendedWishlistWidgetPlugin from './js/wishlist-widget/wishlist-widget.plugin';
-import FilterWidthPlugin from './js/filter-width/filter-width.plugin'
 
 const PluginManager = window.PluginManager;
 
@@ -11,8 +9,3 @@ PluginManager.register('NewsletterPrivacy', NewsletterPrivacy, '.footer-newslett
 PluginManager.register('InputSpinner', InputSpinner, '[data-input-spinner]');
 PluginManager.register('TogglePassword', TogglePassword, '[data-toggle-password]');
 PluginManager.register('RegisterExpand', RegisterExpand, '[data-register-expand]');
-PluginManager.register('FilterWidth', FilterWidthPlugin, '.cms-element-sidebar-filter')
-
-if (window.wishlistEnabled) {
-    PluginManager.override('WishlistWidget', ExtendedWishlistWidgetPlugin, '[data-wishlist-widget]');
-}
